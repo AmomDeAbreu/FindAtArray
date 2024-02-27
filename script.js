6ab4d270e3d49ad554a1df09f3af2a6e10ced4c1
Amom Ao Cubo <102225054+AmomDeAbreu@users.noreply.github.com>
2024-02-27 09:20:18 -0300

    Started comparing numbers, but then I changed to strings

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const array1 = [11,25,3,43,59,6,72,18,29,10,10];
+const array1 = ["Amom", "Albuquerque", "Abigail", "Alisson"];
 let input1 = document.querySelector(".in_txt");
 const btn = document.querySelector("#btn3");
 const res = document.querySelector("#resultado");
@@ -8,9 +8,11 @@ a1.innerText = "Array: [" + array1 + "]";
 
 
 btn.addEventListener("click", () => {
-    let n1 = Number(input1.value);
-    let index = array1.findIndex((el) => {
-        return el == n1;
+    let n1 = input1.value.toString();
+    let index = array1.findIndex((e) => {
+        e.toString();
+        console.log(typeof e, typeof n1);
+        return e.toLowerCase() === n1.toLowerCase();
     });
     
     res.innerHTML = "Valor encontrado na posição " + index + "!";
